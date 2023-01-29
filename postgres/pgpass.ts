c4cbf1c08d5c83e563b04d3f681100c11286c6d7
Shahid N. Shah <shahid@shah.org>
2023-01-29 15:36:30 -0500

    refactor: version should be Aide-independent

--- postgres/pgpass.ts
+++ postgres/pgpass.ts
@@ -10,7 +10,7 @@ import { Connection, parse } from "./pgpass-parse.ts";
 //       trusted machines so be careful. Find a string replacement library to
 //       upgrade later.
 
-const version = "0.1.3";
+const version = "1.0.0";
 const cmd = "pgpass";
 const pgPassFile = path.join(`${Deno.env.get("HOME")}`, ".pgpass");
 const doc = `
